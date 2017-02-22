c5c96051371a81851c6320b0186e54bba92ecd95
Wes Rich <wes@wesrich.com>
2017-02-22 15:47:49 -0500

    Temporarily empty out some older implementation details.

--- lib/fhir_models/bootstrap/json.rb
+++ lib/fhir_models/bootstrap/json.rb
@@ -1,25 +1,7 @@
 module FHIR
   module Json
-    #
-    #  This module includes methods to serialize or deserialize FHIR resources to and from JSON.
-    #
-
-    def to_json
-      JSON.pretty_unparse(to_hash)
-    end
-
-    def self.from_json(json)
-      hash = JSON.parse(json)
-      resource = nil
-      begin
-        resource_type = hash['resourceType']
-        klass = Module.const_get("FHIR::#{resource_type}")
-        resource = klass.new(hash)
-      rescue => e
-        FHIR.logger.error("Failed to deserialize JSON:\n#{json}\n#{e.backtrace}")
-        resource = nil
-      end
-      resource
+    def self.from_json(contents)
+      FHIR::Model.from_json(contents)
     end
   end
 end
